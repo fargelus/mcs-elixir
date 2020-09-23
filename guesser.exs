ccb11570c462fc6404fa96b71aa1965af68fa924
Dmitriy Nemykin <ddraudred@gmail.com>
2020-09-23 19:03:36 +0300

    Cleanup

--- guesser.exs
+++ guesser.exs
@@ -1,7 +1,6 @@
-limit = IO.gets("Enter limit number: ")
-{limit, _} = Integer.parse(limit)
+{limit, _} = Integer.parse(IO.gets("Enter limit number: "))
 
 random_number = :rand.uniform(limit)
-{user_number, _} = Integer.parse(IO.gets("Guess the number: "))
-^random_number = user_number
+{^random_number, _} = Integer.parse(IO.gets("Guess the number: "))
+
 IO.puts("You was right! The number was: #{random_number}")
