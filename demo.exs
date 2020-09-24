743e5af6e3e44fe7488e8534ec70b067c185c9ed
Dmitriy Nemykin <ddraudred@gmail.com>
2020-09-24 20:10:41 +0300

    Guards

--- demo.exs
+++ demo.exs
@@ -5,9 +5,18 @@ defmodule Demo do
 
   def mult(arg), do: arg * 2
 
-  def division(arg, 0), do: "Infinity"
-
+  def division(_, 0), do: "Infinity"
   def division(arg, d \\ 2), do: arg / d
+
+  def fact(n) when n < 0 or not is_integer(n) do
+    IO.puts("Wrong argument #{n}")
+  end
+
+  def fact(0), do: 1
+  def fact(n), do: n * fact(n - 1)
 end
 
 Demo.my_function(3) |> IO.inspect
+Demo.fact(5) |> IO.inspect
+Demo.fact(-5) |> IO.inspect
+Demo.fact('sdfsdfsdf') |> IO.inspect
