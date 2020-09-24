6c3b6363e83133f7f7360802e276ecd44b776880
Dmitriy Nemykin <ddraudred@gmail.com>
2020-09-24 19:56:13 +0300

    Arity test

--- demo.exs
+++ demo.exs
@@ -1,11 +1,13 @@
 defmodule Demo do
   def my_function(b) do
-    (b + 2) |> mult |> div
+    (b + 2) |> mult |> division(0)
   end
 
-  def mult(arg), do: arg * 2  
+  def mult(arg), do: arg * 2
 
-  def div(arg), do: arg / 2
+  def division(arg, 0), do: "Infinity"
+
+  def division(arg, d \\ 2), do: arg / d
 end
 
 Demo.my_function(3) |> IO.inspect
