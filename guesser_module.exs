21d30da433b42fc272d5e53e012e235f79c4867d
Dmitriy Nemykin <ddraudred@gmail.com>
2020-09-28 16:06:36 +0300

    Refactoring

--- guesser_module.exs
+++ guesser_module.exs
@@ -1,23 +1,37 @@
 defmodule Guesser do
   def start do
-    random = IO.gets("Enter limit number: ") |> Integer.parse |> random_number
-    random |> IO.inspect
-    IO.gets("Guess the number: ") |> Integer.parse |> check(random)
-    exit(:shutdown)
+    number = read_input("Enter limit number: ")
+    number |> random |> check(number)
   end
 
-  def random_number({limit, _}), do: :rand.uniform(limit)
-
-  def random_number(_) do
-    IO.ANSI.red() <> "Please, enter valid number!" <> IO.ANSI.reset() |> IO.puts
-    start()
+  defp read_input(msg) do
+    IO.gets(msg) |> Integer.parse |> to_int
   end
 
-  def check({guess_number, _}, random) when guess_number == random do
-    IO.ANSI.green() <> "You was right! The number was: #{random}" <> IO.ANSI.reset() |> IO.puts
+  defp to_int({number, _}), do: number
+
+  defp to_int(_), do: read_input("Please enter the valid number: ")
+
+  defp error(msg) do
+    IO.ANSI.red() <> msg <> IO.ANSI.reset() |> IO.puts
   end
 
-  def check({_, _}, random), do: IO.puts "You wrong. The number was #{random}"
+  defp random(limit) when limit > 0 do
+    num = :rand.uniform(limit)
+    IO.puts "I set #{num}"
+    num
+  end
+
+  defp random(_) do
+    error("Please enter an integer number greater than 0")
+    read_input("Enter limit number: ") |> random()
+  end
+
+  defp check(number, number) do
+    IO.ANSI.green() <> "You was right! The number was: #{number}" <> IO.ANSI.reset() |> IO.puts
+  end
+
+  defp check(_number, random), do: error("You wrong. The number was #{random}")
 end
 
 Guesser.start
