93d4e023f09fc055793520528cda8fb336881aa4
Dmitriy Nemykin <ddraudred@gmail.com>
2020-10-03 17:48:01 +0300

    Mult function added

--- examples/traverse.exs
+++ examples/traverse.exs
@@ -11,7 +11,14 @@ defmodule Demo do
   def map([head | tail], cb) do
     [cb.(head) | map(tail, cb)]
   end
+
+  def mult([]), do: 1
+
+  def mult([head|tail]) do
+    head * mult(tail)
+  end
 end
 
 Demo.traverse([1, 2, 3, 4, 5])
-Demo.map([1, 2, 3], fn(el) -> el * 2 end) |> IO.inspect
+Demo.map([1, 2, 3], &(&1 * 2)) |> Demo.mult |> IO.inspect
+Demo.mult([1, 2, 3]) |> IO.inspect
